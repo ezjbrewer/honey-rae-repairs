11e1f2f29e61dd38c75bb89b2e78ea238d7ef5fc
Ezra Brewer <ezjbrewer@gmail.com>
2024-02-26 14:00:26 -0600

    updated to tutorial

---
+++ src/components/tickets/TicketList.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react"
+import { Ticket } from "./Ticket.jsx"
+import { getAllTickets } from "../../services/ticketService.jsx"
+import "./Tickets.css"
+import { FilterBar } from "./Filter-Bar.jsx"
+
+
+
+export const TicketList = () => {
+  const [allTickets, setAllTickets] = useState([])
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
+  const [filteredTickets, setFilteredTickets] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  
+  useEffect(() => {
+    getAllTickets.apply().then((ticketsArray) => {
+      setAllTickets(ticketsArray)
+      console.log("ticket")
+    })
+  }, []) // runs on initial render of component
+  
+  useEffect(() => {
+    if (showEmergencyOnly) {
+      const emergencyTickets = allTickets.filter(ticket => ticket.emergency === true)
+      setFilteredTickets(emergencyTickets)
+    } else {
+      setFilteredTickets(allTickets)
+    }
+  }, [showEmergencyOnly, allTickets])
+  
+  useEffect(() => {
+    const foundTickets = allTickets.filter((ticket) =>
+      ticket.description.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      setFilteredTickets(foundTickets)
+  }, [searchTerm, allTickets])
+  
+  return (
+    <div className="tickets-container">
+      <h2>Tickets</h2>
+      <FilterBar
+        setShowEmergencyOnly={setShowEmergencyOnly}
+        setSearchTerm={setSearchTerm}
+      />
+      <article className="tickets">
+        {filteredTickets.map(ticketObject => {
+          return (
+          <Ticket ticket={ticketObject} key={ticketObject.id}/>
+          )
+        })}
+      </article>
+</div>
+)
+}
+
